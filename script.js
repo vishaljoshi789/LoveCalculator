64cda287bd8bdd99d7bcb50bb98d8dcfdde1f0bc
Vishal <vishalwebdev0987@gmail.com>
2022-01-12 13:31:19 +0530

    love cata functionality added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 name1 = document.getElementsByName("name1")[0];
 name2 = document.getElementsByName("name2")[0];
+loveCata = ["Romantic", "Innocent", "Loyal", "Possessive", "Unconditional", "Mutual Respect", "Selfless", "Pure", "Trust", "Emotional"]
 
 calculateLove = function() {
     n1 = name1.value.toLowerCase();
@@ -9,11 +10,6 @@ calculateLove = function() {
     else{
         tens = 0
         ones = 0
-        romantic = 0
-        emotional = 0
-        trust = 0
-        loyal = 0
-        innosent = 0
         lst1 = ['t', 'r', 'u', 'e']
         lst1.forEach((i)=>{
             if (n1.includes(i)) {
@@ -34,38 +30,21 @@ calculateLove = function() {
         })
         if(tens<2){
             tens+=8
-            romantic = 90 - ones + 6
-            emotional = 80 - ones + 9
-            trust = 90 - ones + 2
-            loyal = 90 - ones + 7
-            innosent = 80 - ones + 7
         }
         else if(tens<3){
             tens = 8
-            romantic = 80 - ones + 9
-            emotional = 90 - ones + 6
-            trust = 80 - ones + 8
-            loyal = 90 - ones + 4
-            innosent = 90 - ones + 1
         }
         else if(tens<5){
             tens = 4
-            romantic = 80 - ones + 5
-            emotional = 80 - ones + 9
-            trust = 80 - ones + 3
-            loyal = 90 - ones + 2
-            innosent = 90 - ones + 4
         }
         else if(tens>=10){
             tens -= 1
 
         }
+        loveper = tens*10+ones
+        lovecata_index = ones>=3 ? [ones, ones-1, ones-2, ones-3] : [ones, ones+1, ones+2, ones+3]
+        lovecata_index.forEach((i)=> document.querySelector('.love-cata').innerHTML += `<li>${loveCata[i]}</li>`)
         document.querySelector('.my-chart').innerHTML = '<canvas id="loveper"></canvas>'
-        document.querySelector('.love-cata').innerHTML = `<div><p>Romantic</p><canvas id="romantic-love"></canvas></div>
-        <div><p>Emotional</p><canvas id="emotional-love"></canvas></div>
-        <div><p>Trust</p><canvas id="trust-love"></canvas></div>
-        <div><p>Loyal</p><canvas id="loyal-love"></canvas></div>
-        <div><p>Innocent</p><canvas id="innocent-love"></canvas></div>`
         const lovechart = document.getElementById('loveper').getContext('2d');
         const myChart = new Chart(lovechart, {
         type: 'doughnut',
@@ -84,92 +63,8 @@ calculateLove = function() {
             maintainAspectRatio: false
             },
         })
-        const romanticLove = new Chart(document.getElementById('romantic-love').getContext('2d'), {
-            type: 'doughnut',
-            data: {
-                datasets: [{
-                    label: 'Romantic',
-                    data: [romantic, 100-romantic],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)','rgb(255, 255, 255)',
-                      ],
-                      hoverOffset: 10
-                  }]
-                },
-            options: {
-                responsive: true,
-                // maintainAspectRatio: false
-                },
-            })
-        const emotionalLove = new Chart(document.getElementById('emotional-love').getContext('2d'), {
-            type: 'doughnut',
-            data: {
-                datasets: [{
-                    label: 'Emotional',
-                    data: [emotional, 100-emotional],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)','rgb(255, 255, 255)',
-                      ],
-                      hoverOffset: 10
-                  }]
-                },
-            options: {
-                responsive: true,
-                // maintainAspectRatio: false
-                },
-            })
-        const trustLove = new Chart(document.getElementById('trust-love').getContext('2d'), {
-            type: 'doughnut',
-            data: {
-                datasets: [{
-                    label: 'Trust',
-                    data: [trust, 100-trust],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)','rgb(255, 255, 255)',
-                      ],
-                      hoverOffset: 10
-                  }]
-                },
-            options: {
-                responsive: true,
-                // maintainAspectRatio: false
-                },
-            })
-        const loyalLove = new Chart(document.getElementById('loyal-love').getContext('2d'), {
-            type: 'doughnut',
-            data: {
-                datasets: [{
-                    label: 'Loyal',
-                    data: [loyal, 100-loyal],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)','rgb(255, 255, 255)',
-                      ],
-                      hoverOffset: 10
-                  }]
-                },
-            options: {
-                responsive: true,
-                // maintainAspectRatio: false
-                },
-            })
-        const innocentLove = new Chart(document.getElementById('innocent-love').getContext('2d'), {
-            type: 'doughnut',
-            data: {
-                datasets: [{
-                    label: 'Innocent',
-                    data: [innosent, 100-innosent],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)','rgb(255, 255, 255)',
-                      ],
-                      hoverOffset: 10
-                  }]
-                },
-            options: {
-                responsive: true,
-                // maintainAspectRatio: false
-                },
-            })
-        document.querySelector('.love-percentage').innerHTML = `Love Percentage = ${String(tens) + String(ones)}%`     
+        document.querySelector('.love-percentage').innerHTML = `Love Percentage = ${loveper}%` 
+
         fetch('https://g.tenor.com/v1/search?q=loveyou&key=LFKOLDCGEUVU&limit=50').then(response => response.json()).then(data => {document.querySelector('#gif').innerHTML = `<img src="${data.results[randomno].media[0].gif.url}" alt="">`})
 
     }
